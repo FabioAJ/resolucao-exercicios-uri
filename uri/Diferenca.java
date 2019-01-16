2cbb8a6911ffb8b75bfc1413c26b4f901dd9ad03
FabioAJ <fabioaugusto.aj@gmail.com>
2019-01-16 17:22:00 -0200

    Resolve exercicio URI1007

--- uri/Diferenca.java
+++ uri/Diferenca.java
@@ -1,8 +1,8 @@
 /*
-Nome do autor: Fábio Augusto Amaro Jaime
-Data de criação do arquivo: 16/01/2019
-Objetivo sucinto do programa: Calcula uma diferença de inteiros
-Referência ao enunciado/origem do exercício: https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
+Nome do autor: Fï¿½bio Augusto Amaro Jaime
+Data de criaï¿½ï¿½o do arquivo: 16/01/2019
+Objetivo sucinto do programa: Calcula uma diferenÃ§a de inteiros
+Referï¿½ncia ao enunciado/origem do exercï¿½cio: https://www.urionlinejudge.com.br/judge/pt/problems/view/1007
 */
 
 import java.util.Scanner;
@@ -10,14 +10,14 @@ import java.util.Scanner;
 public class Diferenca {
 
 	public static void main(String[] args) {
-		
+
 		Scanner entradaDados = new Scanner(System.in);
-		
+
 		int a = entradaDados.nextInt();;
 		int b = entradaDados.nextInt();;
 		int c = entradaDados.nextInt();;
 		int d = entradaDados.nextInt();;
-		
+
 		System.out.println("DIFERENCA = " + (a * b - c * d));
 		entradaDados.close();
 	}
